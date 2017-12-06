f4bcc4671a3d326cb896e22d3935dfb92af1b958
victormf123 <victormf.df@hotmail.com>
2017-12-06 19:22:46 -0200

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+import Camera from 'react-native-camera';
+
+export default class App extends Component {
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+  onBarCodeRead(data) {
+    alert(data.data);
+    console.log("Barcode: ", data);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          barCodeTypes={[
+          	Camera.constants.BarCodeType.qr,
+          ]}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[Take picture]</Text>
+        </Camera>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
